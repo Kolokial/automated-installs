06e8ff77f069c4da34c59f87952fac1b1cc6c0c3
koloqial <koloqial@icloud.com>
2024-09-29 21:21:36 +0100

    Tidy up and added .net packages

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -1,9 +1,11 @@
 Write-Host "Checking if Chocolatey is installed..."
 $isChocoInstalled = Get-Command -Name choco.exe -ErrorAction SilentlyContinue
 
-if($null -eq$isChocoInstalled){
+if ($null -eq $isChocoInstalled) {
     Write-Host "Nope, it's not...Let's install that now!"
-    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    Set-ExecutionPolicy Bypass -Scope Process -Force; 
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; 
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 }
 
 Write-Host "Yes, it is. Let's continue..."
@@ -12,3 +14,6 @@ choco install packages.config -y
 
 Write-Host "Okay, let's setup WSL with Debian."
 wsl --install -d Debian
+
+Write-Host "Okay, let's install DotNet packages"
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://dot.net/v1/dotnet-install.ps1'))
